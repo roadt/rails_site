b04a8d06e4127b466709a2c7d97f1b69565b4627
Road Tang <roadtang@gmail.com>
2013-04-22 09:12:18 +0800

    transit database from sqilte to mysql

--- Gemfile
+++ Gemfile
@@ -6,7 +6,7 @@ gem 'rails', '3.2.8'
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
 gem 'sqlite3'
-
+gem 'mysql2'
 
 # Gems used only for assets and not required
 # in production environments by default.
@@ -21,7 +21,7 @@ group :assets do
   gem 'uglifier', '>= 1.0.3'
 end
 
-gem 'jquery-rails'
+gem 'jquery-rails', '~>2.1'
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- config/deploy.rb
+++ config/deploy.rb
@@ -12,9 +12,9 @@ set :use_sudo, false
 role :web, "venus"                          # Your HTTP server, Apache/etc
 role :app,"venus"                         # This may be the same as your `Web` server
 role :db,  "venus", :primary => true # This is where Rails migrations will run
-role :db, "oldman"
-role :web, "oldman"
-role :app, "oldman"
+#role :db, "oldman"
+#role :web, "oldman"
+#role :app, "oldman"
 
 # if you want to clean up old releases on each deploy uncomment this:
 after "deploy:restart", "deploy:cleanup"
@@ -35,8 +35,6 @@ after :bundle_gems, :deploy
 
 # If you are using Passenger mod_rails uncomment this:
 namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
 #   task :restart, :roles => :app, :except => { :no_release => true } do
 #     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
 #   end
