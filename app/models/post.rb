2f906a47164a8edbd16fcfd980cb978bcaf40808
Road Tang <roadtang@gmail.com>
2013-09-07 04:46:46 +0800

    add tag suppport to post ( show not yet)

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,11 @@
 class Post < ActiveRecord::Base
-
-  attr_accessible :content, :name, :title
+  
+  attr_accessible :content, :name, :title, :tag_list
 #  validates :name, :presence => true
   validates :title, :presence => true, :length => { :minimum => 5 }
 
   has_many :comments, :dependent => :destroy
+
+  #  taggable
+  acts_as_taggable
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -2,4 +2,7 @@ class Show < ActiveRecord::Base
   attr_accessible :name, :description
 
   has_many :show_data
+
+  # taggable
+  acts_as_taggable
 end
