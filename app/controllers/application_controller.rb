9c854f64a4bec63d8b9ec64f8fc6e2ac3f3a9100
Road Tang <roadtang@gmail.com>
2013-09-08 14:53:04 +0800

    add  error template

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+#  unless Rails.application.config.consider_all_requests_local
+    rescue_from Exception, with: lambda { |e| render_error 500, e}
+#  end
+
+  private
+    def render_error(status, err)
+      respond_to do |format|
+        format.html { render template: "errors/error_#{status}",  status: status }
+        format.all { render nothing: true, status: status }
+      end
+    end
 end
