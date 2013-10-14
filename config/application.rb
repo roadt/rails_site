f0f4096c5f9a81e1258a7750ca61570ee28391af
Road Tang <roadtang@gmail.com>
2013-10-15 02:11:14 +0800

    make factory_girl default fixture engine

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,9 @@ module Blog
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    config.generators do |g|
+      g.fixture_replacement :factory_girl
+    end
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
 
