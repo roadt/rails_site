9e5ad0b38a79516c0a42f90145004cb784b698d6
Road Tang <roadtang@gmail.com>
2013-10-20 10:55:30 +0800

    add profile support and protected profile.json with oauth token

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,25 +1,27 @@
 class ProfilesController < ApplicationController
-  before_filter :authenticate_user!
-  # GET /profiles/1
-  # GET /profiles/1.json
-  respond_to :json
 
+  before_filter :authenticate_user!
+
+  respond_to :json, :xml
+
+  doorkeeper_for :show, :if => lambda {|c| c.params[:format] == 'json'}
+
+  # GET /profiles.json
   def show
     @profile = current_user #Profile.find(params[:id])
-
-    respond_to do |format|
+    respond_with @profile do |format|
       format.html # show.html.erb
-      format.json { render json: @profile }
+#      format.json { render json: @profile }
     end
   end
 
-  # GET /profiles/1/edit
+  # GET /profiles/edit
   def edit
     @profile = current_user
   end
 
-  # PUT /profiles/1
-  # PUT /profiles/1.json
+  # PUT /profiles
+  # PUT /profiles
   def update
     @profile = current_user
 
@@ -32,6 +34,10 @@ class ProfilesController < ApplicationController
         format.json { render json: @profile.errors, status: :unprocessable_entity }
       end
     end
+  end
+
+  def doorkeeper_unauthorized_render_options
+    {:template => "errors/error_401"}
   end
 
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,6 +9,7 @@ require 'rspec/autorun'
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 RSpec.configure do |config|
+
   # ## Mock Framework
   #
   # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
@@ -35,4 +36,8 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+
+  # Devise Test support
+  config.include Devise::TestHelpers, :type => :controller
+
 end
