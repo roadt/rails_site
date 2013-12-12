3d1c81d51a65783a39a1a27fb0f67b2e4cd677ac
Road Tang <roadtang@gmail.com>
2013-12-12 23:02:59 +0800

    when app run behind nginx, the oauth callback mismatch,  provider give 'invalid_crendential'. fix it to read from nginx header. note, nginx need 1, give servername for server 2, add 'proxy_set_header X-Real-Base $scheme://$server_name:$server_port;' to server section

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,3 +3,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, SECRETS['oauth2']['google']['key'],  SECRETS['oauth2']['google']['secret']
   provider :github,  SECRETS['oauth2']['github']['key'], SECRETS['oauth2']['github']['secret']
 end
+OmniAuth.config.full_host  = lambda {|env|
+  "http://radt.dyndns.org:8000"
+}
