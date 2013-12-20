fda222719e6a525633375d51868452a3dba4ba96
Road Tang <roadtang@gmail.com>
2013-12-21 01:33:56 +0800

    refine omniauth reverse proxy support

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,5 +4,11 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github,  SECRETS['oauth2']['github']['key'], SECRETS['oauth2']['github']['secret']
 end
 OmniAuth.config.full_host  = lambda {|env|
-  "http://radt.dyndns.org:8000"
+  real_base_uri = 'HTTP_X_REAL_BASE'
+  if env.key?(real_base_uri)  # if proxy send back real base uri
+    env[real_base_uri]
+  else
+    # no proxy, extract from request url
+    Rack::Request.new(env).base_url
+  end
 }
